d182c7f120fc03ea770b977a12385abb0c34713c
Alexander Smirnov <al.smirnov996@gmail.com>
2023-09-28 21:42:16 +0300

    feat(prettier-config): setup new package

---
+++ packages/prettier-config/README.md
@@ -0,0 +1,9 @@
+# @aliexme/prettier-config
+
+Shared Prettier configuration
+
+## Installation
+
+```
+npm i --save-dev prettier @aliexme/prettier-config
+```
--- prettier.config.js
+++ prettier.config.js
@@ -1,14 +1,4 @@
+const prettierConfig = require('@aliexme/prettier-config')
+
 /** @type {import('prettier').Config} */
-module.exports = {
-  printWidth: 120,
-  tabWidth: 2,
-  useTabs: false,
-  semi: false,
-  singleQuote: true,
-  quoteProps: 'as-needed',
-  jsxSingleQuote: true,
-  trailingComma: 'all',
-  bracketSpacing: true,
-  bracketSameLine: false,
-  arrowParens: 'always',
-}
+module.exports = prettierConfig
