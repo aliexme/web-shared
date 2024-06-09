89ce216e5493037de5481123b1507a67f4a528e8
Alexander Smirnov <al.smirnov996@gmail.com>
2024-06-09 15:13:55 +0300

    feat(stylelint-config): adjust selector-class-pattern rule

--- packages/stylelint-config/rules/base.js
+++ packages/stylelint-config/rules/base.js
@@ -3,5 +3,6 @@ module.exports = {
   extends: ['stylelint-config-standard'],
   rules: {
     'at-rule-no-unknown': [true, { ignoreAtRules: ['tailwind'] }],
+    'selector-class-pattern': ['^([a-z][a-zA-Z0-9]*)([-_][a-z0-9]+)*$'],
   },
 }
