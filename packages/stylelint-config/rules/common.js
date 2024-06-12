86e15b966aca7bbd6a590f6c6eb6c02198f0a787
Alexander Smirnov <al.smirnov996@gmail.com>
2024-06-12 13:06:34 +0300

    fix(stylelint-config): "selector-pseudo-class-no-unknown" rule

--- packages/stylelint-config/rules/common.js
+++ packages/stylelint-config/rules/common.js
@@ -2,5 +2,6 @@
 module.exports = {
   rules: {
     'selector-class-pattern': ['^([a-z][a-zA-Z0-9]*)([-_][a-z0-9]+)*$'],
+    'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global'] }],
   },
 }
