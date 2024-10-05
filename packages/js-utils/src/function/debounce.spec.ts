2fbbda0998d2af6c7ef918bcce48478bb14c96bb
Alexander Smirnov <al.smirnov996@gmail.com>
2024-10-05 21:22:15 +0300

    feat(js-utils): improve debounce function

--- packages/js-utils/src/function/debounce.spec.ts
+++ packages/js-utils/src/function/debounce.spec.ts
@@ -46,4 +46,55 @@ describe('debounce', () => {
     jest.advanceTimersByTime(600)
     expect(func).toHaveBeenCalledTimes(0)
   })
+
+  it('should debounce function with max wait time', () => {
+    jest.useFakeTimers()
+
+    const func = jest.fn(noop)
+    const debouncedFunc = debounce(func, 1000, { maxWait: 1500 })
+
+    debouncedFunc()
+    debouncedFunc()
+    debouncedFunc()
+    expect(func).toHaveBeenCalledTimes(0)
+
+    jest.advanceTimersByTime(600)
+    debouncedFunc()
+    expect(func).toHaveBeenCalledTimes(0)
+
+    jest.advanceTimersByTime(600)
+    debouncedFunc()
+    expect(func).toHaveBeenCalledTimes(0)
+
+    jest.advanceTimersByTime(600)
+    expect(func).toHaveBeenCalledTimes(1)
+  })
+
+  it('should debounce function with leading', () => {
+    jest.useFakeTimers()
+
+    const func = jest.fn(noop)
+    const debouncedFunc = debounce(func, 1000, { leading: true })
+
+    debouncedFunc()
+    expect(func).toHaveBeenCalledTimes(1)
+
+    jest.advanceTimersByTime(1200)
+    expect(func).toHaveBeenCalledTimes(1)
+
+    debouncedFunc()
+    debouncedFunc()
+    debouncedFunc()
+    expect(func).toHaveBeenCalledTimes(2)
+
+    jest.advanceTimersByTime(600)
+    debouncedFunc()
+    expect(func).toHaveBeenCalledTimes(2)
+
+    jest.advanceTimersByTime(600)
+    expect(func).toHaveBeenCalledTimes(2)
+
+    jest.advanceTimersByTime(600)
+    expect(func).toHaveBeenCalledTimes(3)
+  })
 })
--- packages/js-utils/src/function/debounce.ts
+++ packages/js-utils/src/function/debounce.ts
@@ -1,27 +1,65 @@
+import { clamp } from '../number/clamp'
+
 export interface DebouncedFunc<T extends (...args: unknown[]) => unknown> {
   (...args: Parameters<T>): void
   cancel(): void
 }
 
-export const debounce = <T extends (...args: unknown[]) => unknown>(func: T, wait: number): DebouncedFunc<T> => {
-  let timeoutId: ReturnType<typeof setTimeout> | null
+export interface DebounceOptions {
+  maxWait?: number
+  leading?: boolean
+}
 
-  const cancelTimeout = () => {
-    if (timeoutId !== null) {
-      clearTimeout(timeoutId)
-      timeoutId = null
+export const debounce = <T extends (...args: unknown[]) => unknown>(
+  func: T,
+  wait: number,
+  options: DebounceOptions = {},
+): DebouncedFunc<T> => {
+  const { maxWait, leading } = options
+
+  let timerId: ReturnType<typeof setTimeout> | null = null
+  let firstCallTime: number | null = null
+
+  const resetTimer = () => {
+    if (timerId !== null) {
+      clearTimeout(timerId)
+      timerId = null
     }
   }
 
-  const debouncedFunc: DebouncedFunc<T> = (...args) => {
-    cancelTimeout()
-
-    timeoutId = setTimeout(() => {
-      func(...args)
-    }, wait)
+  const resetFirstCall = () => {
+    firstCallTime = null
   }
 
-  debouncedFunc.cancel = cancelTimeout
+  const cancelFuncCall = () => {
+    resetTimer()
+    resetFirstCall()
+  }
+
+  const debouncedFunc: DebouncedFunc<T> = (...args) => {
+    resetTimer()
+
+    const now = Date.now()
+    const timeSinceFirstCall = firstCallTime ? now - firstCallTime : 0
+    const remainingWait = maxWait ? clamp(maxWait - timeSinceFirstCall, 0, wait) : wait
+
+    if (!firstCallTime) {
+      firstCallTime = now
+
+      if (leading) {
+        func(...args)
+        timerId = setTimeout(resetFirstCall, remainingWait)
+        return
+      }
+    }
+
+    timerId = setTimeout(() => {
+      func(...args)
+      resetFirstCall()
+    }, remainingWait)
+  }
+
+  debouncedFunc.cancel = cancelFuncCall
 
   return debouncedFunc
 }
