5ab1397ec63a87a4c1ed7e472d8ce7d3b8f785fa
Alexander Smirnov <al.smirnov996@gmail.com>
2024-11-17 13:18:41 +0300

    feat(react-utils): add useEventListener hook

---
+++ packages/react-utils/src/hooks/useEventListener.ts
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+
+export interface UseEventListenerOptions extends AddEventListenerOptions {
+  enabled?: boolean
+}
+
+export const useEventListener = (
+  target: EventTarget,
+  type: string,
+  listener: EventListenerOrEventListenerObject,
+  options: UseEventListenerOptions = {},
+) => {
+  const { enabled = true, ...listenerOptions } = options
+
+  useEffect(() => {
+    if (!enabled) {
+      return
+    }
+
+    target.addEventListener(type, listener, listenerOptions)
+
+    return () => {
+      target.removeEventListener(type, listener, listenerOptions)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [target, type, listener, enabled])
+}
