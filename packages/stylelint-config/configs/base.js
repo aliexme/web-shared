06b21748e6a2151d3e22c96cfd65b9527c6d9e8a
Alexander Smirnov <al.smirnov996@gmail.com>
2025-02-22 22:16:33 +0300

    fix(stylelint-config): ignore @apply directive

--- packages/stylelint-config/configs/base.js
+++ packages/stylelint-config/configs/base.js
@@ -3,5 +3,6 @@ module.exports = {
   extends: ['stylelint-config-standard', './common'],
   rules: {
     'at-rule-no-unknown': [true, { ignoreAtRules: ['tailwind'] }],
+    'at-rule-no-deprecated': [true, { ignoreAtRules: ['apply'] }],
   },
 }
