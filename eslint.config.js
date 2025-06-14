8ff4a9ae3ef437635122cba061925f9b97627674
Alexander Smirnov <al.smirnov996@gmail.com>
2025-06-14 11:47:03 +0300

    chore: upgrade dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -13,7 +13,6 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const gitignorePath = path.resolve(__dirname, '.gitignore')
 
-/** @type {import('typescript-eslint').ConfigArray} */
 export default [
   includeIgnoreFile(gitignorePath),
   ...eslintConfigRecommended,
