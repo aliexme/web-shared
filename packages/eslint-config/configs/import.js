17b986b9408ea00e62e5bcc87c52e79114276efe
Alexander Smirnov <al.smirnov996@gmail.com>
2025-06-14 13:47:34 +0300

    feat(eslint): replace eslint-plugin-import with eslint-plugin-import-x

--- packages/eslint-config/configs/import.js
+++ packages/eslint-config/configs/import.js
@@ -1,50 +1,53 @@
-import { defineConfig } from 'eslint/config'
-// @ts-expect-error Could not find a declaration file for module 'eslint-plugin-import'
-import importPlugin from 'eslint-plugin-import'
+import tseslint from 'typescript-eslint'
+import { createNodeResolver, importX } from 'eslint-plugin-import-x'
+import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript'
 
-export default defineConfig([
-  importPlugin.flatConfigs.recommended,
-  importPlugin.flatConfigs.typescript,
-  {
-    name: '@aliexme/eslint-config/import',
-    settings: {
-      'import/resolver': {
-        node: {
-          moduleDirectory: ['src/', 'node_modules'],
-        },
-        typescript: {},
+export default /** @type {import('eslint').Linter.Config[]} */ (
+  tseslint.config([
+    importX.flatConfigs.recommended,
+    importX.flatConfigs.typescript,
+    {
+      name: '@aliexme/eslint-config/import',
+      settings: {
+        'import-x/resolver-next': [
+          createNodeResolver({ modules: ['src/', 'node_modules'] }),
+          createTypeScriptImportResolver(),
+        ],
+        'import-x/extensions': ['.js', '.cjs', '.mjs', '.jsx', '.ts', '.tsx', '.vue', '.astro'],
+        'import-x/ignore': ['node_modules'],
       },
-      'import/extensions': ['.js', '.cjs', '.mjs', '.jsx', '.ts', '.tsx', '.vue', '.astro'],
-      'import/ignore': ['node_modules'],
+      rules: {
+        'import-x/order': [
+          'error',
+          {
+            groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index', 'object'],
+            pathGroups: [
+              { pattern: '{react*,vue*,astro*}', group: 'external', position: 'before' },
+              { pattern: '{next*,nuxt*}', group: 'external', position: 'before' },
+              { pattern: '@aliexme/**', group: 'external', position: 'after' },
+              { pattern: '#*', group: 'external', position: 'after' },
+              { pattern: './**/{styles,*.styles,*.css,*.scss}', group: 'internal', position: 'before' },
+              { pattern: '../**/{styles,*.styles,*.css,*.scss}', group: 'internal', position: 'before' },
+              { pattern: '@/**', group: 'internal', position: 'before' },
+              { pattern: '~/**', group: 'internal', position: 'before' },
+            ],
+            pathGroupsExcludedImportTypes: ['builtin'],
+            'newlines-between': 'always',
+            warnOnUnassignedImports: true,
+            distinctGroup: false,
+          },
+        ],
+        'import-x/no-useless-path-segments': ['error', { noUselessIndex: false }],
+        'import-x/no-duplicates': ['error', { 'prefer-inline': true }],
+        'import-x/no-self-import': 'error',
+        'import-x/no-mutable-exports': 'error',
+        'import-x/first': 'error',
+        'import-x/newline-after-import': 'error',
+        'import-x/default': 'off',
+        'import-x/named': 'off',
+        'import-x/namespace': 'off',
+        'import-x/no-named-as-default-member': 'off',
+      },
     },
-    rules: {
-      'import/no-useless-path-segments': ['error', { noUselessIndex: false }],
-      'import/order': [
-        'error',
-        {
-          groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index', 'object'],
-          pathGroups: [
-            { pattern: '{react*,vue*,astro*}', group: 'external', position: 'before' },
-            { pattern: '{next*,nuxt*}', group: 'external', position: 'before' },
-            { pattern: '@aliexme/**', group: 'external', position: 'after' },
-            { pattern: '#*', group: 'external', position: 'after' },
-            { pattern: './**/{styles,*.styles,*.css,*.scss}', group: 'internal', position: 'before' },
-            { pattern: '../**/{styles,*.styles,*.css,*.scss}', group: 'internal', position: 'before' },
-            { pattern: '@/**', group: 'internal', position: 'before' },
-            { pattern: '~/**', group: 'internal', position: 'before' },
-          ],
-          pathGroupsExcludedImportTypes: ['builtin'],
-          'newlines-between': 'always',
-          warnOnUnassignedImports: true,
-          distinctGroup: false,
-        },
-      ],
-      'import/no-duplicates': ['error', { 'prefer-inline': true }],
-      'import/no-self-import': 'error',
-      'import/no-mutable-exports': 'error',
-      'import/first': 'error',
-      'import/newline-after-import': 'error',
-      'import/namespace': 'off',
-    },
-  },
-])
+  ])
+)
