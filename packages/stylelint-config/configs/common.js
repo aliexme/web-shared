44c67bc455ae91fbf990afec78c37a7bca6a04ca
Alexander Smirnov <al.smirnov996@gmail.com>
2025-06-15 13:34:32 +0300

    fix(stylelint-config): adjust layer-name-pattern rule

--- packages/stylelint-config/configs/common.js
+++ packages/stylelint-config/configs/common.js
@@ -3,5 +3,6 @@ export default {
   rules: {
     'selector-class-pattern': ['^([a-z][a-zA-Z0-9]*)([-_][a-z0-9]+)*$'],
     'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global'] }],
+    'layer-name-pattern': '^[a-z][a-z0-9.-]*$',
   },
 }
