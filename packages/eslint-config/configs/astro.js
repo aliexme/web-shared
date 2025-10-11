fb2ee8849de830ab34121d5a3b84b151127b0179
Alexander Smirnov <al.smirnov996@gmail.com>
2025-10-11 23:54:29 +0300

    feat(eslint-config): remove astro/prefer-class-list-directive rule

--- packages/eslint-config/configs/astro.js
+++ packages/eslint-config/configs/astro.js
@@ -13,7 +13,6 @@ export default defineConfig([
     },
     rules: {
       'astro/no-unused-css-selector': 'error',
-      'astro/prefer-class-list-directive': 'error',
       'astro/prefer-object-class-list': 'error',
       'astro/prefer-split-class-list': ['error', { splitLiteral: true }],
     },
